0c407ed9ae2bafecc1043a883b740b45c8b0f011
Nicolas Conaway <n.conaway00@gmail.com>
2020-04-13 21:31:14 -0400

    JavaScript week 4 challenges

---
+++ JavaScript_Week4/veryHard.js
@@ -0,0 +1,9 @@
+//make an array of any given coins
+//[1, 2, 5];
+//create a target that could be set to anything
+//target = 11;
+//make a function with an if statement
+//if target is greater than 0 then lead into a for loop
+//loop will calc the given values in the array to equal the target
+//once calculated the code will end with a number 3 spit out signifying the amount of coins used
+//if else the target is 0 and below, console.log that the value -1 beacuse the function cant add anything up to get 0
